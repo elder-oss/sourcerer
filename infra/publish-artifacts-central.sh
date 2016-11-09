1f7f0ac54dd998902aa35c24abf4212ec46fb6ab
Kristian Freed <kristian@elder.org>
2016-11-09 10:36:41 +0000

    Changes to support building without Elder customizations (still using Elder secrets in deployments)

--- infra/publish-artifacts-central.sh
+++ infra/publish-artifacts-central.sh
@@ -2,7 +2,7 @@
 export TERM="dumb"
 set -e
 echo $ELDER_KEYRING | base64 --decode > /tmp/keyring
-./gradlew uploadArchives closeRepository
+./gradlew uploadArchives closeRepository -Penable_signing=true
 
 echo "Waiting for Nexus to close staging repository"
 sleep 10
