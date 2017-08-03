6f8e34c0fff88dbf97247696096f16347f84dea5
Daniel Josefsson <daniel.j@elder.org>
2017-08-03 18:33:04 +0100

    Fix expected version merge
    The functionality was previously unintentionally asymmetric. Merging
    in one direction didn't necessarily result in the same result as merging
    in the other.
    
    This has now been avoided by always comparing in a certain order. The
    order is given by the enums' ordinal number.

--- sourcerer-core/src/main/java/org/elder/sourcerer/ExpectedVersion.java
+++ sourcerer-core/src/main/java/org/elder/sourcerer/ExpectedVersion.java
@@ -51,76 +51,11 @@ public final class ExpectedVersion {
             final ExpectedVersion baseVersion,
             final ExpectedVersion newVersion) {
         if (baseVersion != null && newVersion != null) {
-            switch (baseVersion.getType()) {
-                case ANY:
-                    // Any is always compatible with other expected version
-                    return newVersion;
-                case ANY_EXISTING:
-                    switch (newVersion.getType()) {
-                        case ANY:
-                        case ANY_EXISTING:
-                            return ExpectedVersion.anyExisting();
-                        case EXACTLY:
-                            return newVersion;
-                        case NOT_CREATED:
-                            throw new ConflictingExpectedVersionsException(
-                                    "Cannot merge 'any existing' with 'not created'",
-                                    baseVersion, newVersion);
-                        default:
-                            throw new IllegalArgumentException(
-                                    "Unrecognized version type: " + newVersion.getType());
-                    }
-                case EXACTLY:
-                    switch (newVersion.getType()) {
-                        case ANY:
-                        case ANY_EXISTING:
-                        case EXACTLY:
-                            if (baseVersion.getExpectedVersion()
-                                    != newVersion.getExpectedVersion()) {
-                                throw new ConflictingExpectedVersionsException(
-                                        "Cannot merge 'exactly "
-                                                + baseVersion.getExpectedVersion()
-                                                + "' with 'exactly "
-                                                + newVersion.getExpectedVersion()
-                                                + "'",
-                                        baseVersion, newVersion);
-                            }
-                            return baseVersion;
-                        case NOT_CREATED:
-                            throw new ConflictingExpectedVersionsException(
-                                    "Cannot merge 'exactly "
-                                            + baseVersion.getExpectedVersion()
-                                            + "' with 'not created'",
-                                    baseVersion, newVersion);
-                        default:
-                            throw new IllegalArgumentException(
-                                    "Unrecognized version type: " + newVersion.getType());
-                    }
-                case NOT_CREATED:
-                    switch (newVersion.getType()) {
-                        case ANY:
-                        case NOT_CREATED:
-                            break;
-                        case ANY_EXISTING:
-                            throw new ConflictingExpectedVersionsException(
-                                    "Cannot merge 'not created' with 'any existing'",
-                                    baseVersion, newVersion);
-                        case EXACTLY:
-                            throw new ConflictingExpectedVersionsException(
-                                    "Cannot merge 'not created' with 'exactly "
-                                            + newVersion.getExpectedVersion() + "'",
-                                    baseVersion, newVersion);
-                        default:
-                            break;
-                    }
-                    return baseVersion;
-                default:
-                    break; // Throws, see below
+            if (baseVersion.getType().compareTo(newVersion.getType()) > 0) {
+                return mergeOrdered(newVersion, baseVersion);
+            } else {
+                return mergeOrdered(baseVersion, newVersion);
             }
-            throw new RuntimeException(
-                    "Internal error, unexpected version combination merging "
-                            + baseVersion
-                            + " with " + newVersion);
         } else if (baseVersion != null) {
             return baseVersion;
         } else if (newVersion != null) {
@@ -128,6 +63,67 @@ public final class ExpectedVersion {
         } else {
             return ExpectedVersion.any();
         }
+    }
+
+    /**
+     * Assumes the expected versions are given in ordinal order.
+     */
+    private static ExpectedVersion mergeOrdered(
+            final ExpectedVersion baseVersion,
+            final ExpectedVersion newVersion) {
+        switch (baseVersion.getType()) {
+            case ANY:
+                // Any is always compatible with other expected version
+                return newVersion;
+            case ANY_EXISTING:
+                switch (newVersion.getType()) {
+                    case ANY_EXISTING:
+                        return ExpectedVersion.anyExisting();
+                    case EXACTLY:
+                        return newVersion;
+                    case NOT_CREATED:
+                        throw new ConflictingExpectedVersionsException(
+                                "Cannot merge 'any existing' with 'not created'",
+                                baseVersion, newVersion);
+                    default:
+                }
+                break;
+            case EXACTLY:
+                switch (newVersion.getType()) {
+                    case EXACTLY:
+                        if (baseVersion.getExpectedVersion()
+                                != newVersion.getExpectedVersion()) {
+                            throw new ConflictingExpectedVersionsException(
+                                    "Cannot merge 'exactly "
+                                            + baseVersion.getExpectedVersion()
+                                            + "' with 'exactly "
+                                            + newVersion.getExpectedVersion()
+                                            + "'",
+                                    baseVersion, newVersion);
+                        }
+                        return baseVersion;
+                    case NOT_CREATED:
+                        throw new ConflictingExpectedVersionsException(
+                                "Cannot merge 'exactly "
+                                        + baseVersion.getExpectedVersion()
+                                        + "' with 'not created'",
+                                baseVersion, newVersion);
+                    default:
+                }
+                break;
+            case NOT_CREATED:
+                switch (newVersion.getType()) {
+                    case NOT_CREATED:
+                        return baseVersion;
+                    default:
+                }
+                break;
+            default:
+        }
+        throw new RuntimeException(
+                "Internal error, unexpected version combination merging "
+                        + baseVersion
+                        + " with " + newVersion);
     }
 
     @Override
