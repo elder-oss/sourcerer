fb62b2abf396a5d4a24cf14870072730d456f49c
Kristian Freed <kristian@elder.org>
2019-10-17 10:52:39 +0100

    Adding Prometheus counters for EventStore health probe, incrementing default timeout to 1000ms

--- sourcerer-esjc-spring/src/main/java/org/elder/soucerer/eventstore/EventStoreHealthIndicator.java
+++ sourcerer-esjc-spring/src/main/java/org/elder/soucerer/eventstore/EventStoreHealthIndicator.java
@@ -3,6 +3,8 @@ package org.elder.soucerer.eventstore;
 import com.github.msemys.esjc.AllEventsSlice;
 import com.github.msemys.esjc.EventStore;
 import com.github.msemys.esjc.Position;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Histogram;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
@@ -13,10 +15,25 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 public class EventStoreHealthIndicator extends AbstractHealthIndicator {
-    private static final int DEFAULT_READ_TIMEOUT_MILLIS = 250;
+    private static final int DEFAULT_READ_TIMEOUT_MILLIS = 1000;
     private static final Logger logger = LoggerFactory.getLogger(EventStoreHealthIndicator.class);
+    private static final String ELDER_NAMESPACE = "elder";
+    private static final Histogram latencyHistogram = Histogram.build()
+            .namespace(ELDER_NAMESPACE)
+            .name("eventstore_healthcheck_latency_seconds")
+            .help("Delay in probing EventStore as part of health check")
+            .register();
+
+    private static final Counter probesCounter = Counter.build()
+            .namespace(ELDER_NAMESPACE)
+            .name("eventstore_healthchecks_total")
+            .labelNames("status")
+            .help("Total number of EventStore health checks with status (success or failure)")
+            .register();
+
     private final EventStore eventStore;
     private final int readTimeoutMillis;
+
 
     public EventStoreHealthIndicator(final EventStore eventStore) {
         this(eventStore, DEFAULT_READ_TIMEOUT_MILLIS);
@@ -30,13 +47,24 @@ public class EventStoreHealthIndicator extends AbstractHealthIndicator {
     @Override
     protected void doHealthCheck(final Health.Builder builder) throws Exception {
         logger.debug("Reading Event Store for health check");
-        AllEventsSlice res = completeReadFuture(
-                eventStore.readAllEventsBackward(Position.END, 1, false));
+        final Histogram.Timer latencyTimer = latencyHistogram.startTimer();
+        boolean successful = false;
+        try {
+            AllEventsSlice res = completeReadFuture(
+                    eventStore.readAllEventsBackward(Position.END, 1, false));
 
-        logger.debug("Event store successfully pinged");
-        builder
-                .status(Status.UP)
-                .withDetail("position", res.nextPosition.toString());
+            logger.debug("Event store successfully pinged");
+            builder
+                    .status(Status.UP)
+                    .withDetail("position", res.nextPosition.toString());
+            probesCounter.labels("up").inc();
+            successful = true;
+        } finally {
+            if (!successful) {
+                probesCounter.labels("down").inc();
+            }
+            latencyTimer.observeDuration();
+        }
     }
 
     private <U> U completeReadFuture(final CompletableFuture<U> future) throws Exception {
