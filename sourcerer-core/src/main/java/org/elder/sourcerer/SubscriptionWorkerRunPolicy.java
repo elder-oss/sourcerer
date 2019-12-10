2d19a7f4c04c1a5f3c7eb29727662fa51f873b05
Daniel Josefsson <daniel.j@elder.org>
2019-12-10 15:24:46 +0000

    Add some javadoc

--- sourcerer-core/src/main/java/org/elder/sourcerer/SubscriptionWorkerRunPolicy.java
+++ sourcerer-core/src/main/java/org/elder/sourcerer/SubscriptionWorkerRunPolicy.java
@@ -1,10 +1,21 @@
 package org.elder.sourcerer;
 
 public interface SubscriptionWorkerRunPolicy {
+    /**
+     * Checks whether subscription worker should run.
+     *
+     * @return true if subscription worker should start consuming events.
+     */
     boolean shouldRun();
 
+    /**
+     * Called when consumer has processed a batch without throwing an exception.
+     */
     void lastRunSuccessful();
 
+    /**
+     * Called when consumer has thrown an exception while processing events.
+     */
     void lastRunFailed(Exception ex);
 
     SubscriptionWorkerRunPolicy ALWAYS = new AlwaysRunSubscriptionWorkerRunPolicy();
