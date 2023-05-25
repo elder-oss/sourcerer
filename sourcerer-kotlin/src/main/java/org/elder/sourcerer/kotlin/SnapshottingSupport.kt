bc420a194d5b74fc9954f2f6a3e0f302ce1740e7
Antonio Centeno Romero <antonio@elder.org>
2023-05-25 11:40:51 +0100

    readValue should be use when uncompressing

--- sourcerer-kotlin/src/main/java/org/elder/sourcerer/kotlin/SnapshottingSupport.kt
+++ sourcerer-kotlin/src/main/java/org/elder/sourcerer/kotlin/SnapshottingSupport.kt
@@ -81,7 +81,7 @@ class SnapshottingSupport<STATE>(
     private fun Snapshot<String>.uncompress() : Snapshot<STATE> {
         val data = Base64.getDecoder().decode(state)
         val uncompressed = GZIPInputStream(data.inputStream()).use { it.readBytes() }
-        val state = mapper.convertValue(uncompressed, clazz) as STATE
+        val state = mapper.readValue(uncompressed, clazz) as STATE
         return Snapshot<STATE>(state, streamVersion)
     }
 
